68d5ec08a383d7f2b5d3a9055d3ecae5fda1f422
Evgeny <thankee@yandex.ru>
2019-09-19 16:35:05 +0400

    ///

--- lecture_15-16/src/js/script.js
+++ lecture_15-16/src/js/script.js
@@ -1,3 +1,13 @@
+//============ ПЕРЕМЕННЫЕ > ============//
+
+const URL = {blog: 'https://my-json-server.typicode.com/dahaka-vas/EPM-FE2019/posts'};
+
+//============ < ПЕРЕМЕННЫЕ ============//
+
+
+
+//============ ФУНКЦИИ > ============//
+
 // function getRandom (min, max) {
 //     return Math.floor (Math.random() * (max - min + 1)) + min;
 // }
@@ -7,61 +17,84 @@ function avgRating (r) {
     return r;
 }
 
-// function Renderable () {}
+//============ < ФУНКЦИИ ============//
 
-// Object.assign(Renderable.prototype, {
-//     render() {
-//         throw new Error('Метода Render не существует')
-//     }
-// })
 
-class Posts {
+
+//============ КЛАССЫ > ============//
+
+class ServerApi {
     constructor(url) {
-        this.posts = fetch(url)
+        this.url = url;
+    }
+
+    getDataFromServer (url) {
+        return fetch(url)
         .then(response => response.json())
         .catch(error => alert(error));
     }
 }
 
-class Blog extends Posts {
+class Renderable {
+    render () {
+        throw new Error('Метода Render не существует')
+    }
+}
+
+class Blog extends Renderable {
+    constructor() {
+        super();
+        // this.posts = this.fillPosts();
+        this.posts = new ServerApi().getDataFromServer(URL.blog);
+    }
+
+    fillPosts () {
+        // new ServerApi(URL.blog).getDataFromServer().then(data => this.posts = data);
+        let posts = new ServerApi(URL.blog).getDataFromServer();
+        return posts;
+    }
+
     avgRatingSort () {
-        this.posts.map((item, i) => item.avgRating = avgRating (this.posts[i].ratings));
-        this.posts.sort((a, b) => b.avgRating - a.avgRating);
+        this.posts.then(data => {
+            data.map((item, i) => item.avgRating = avgRating (data[i].ratings));
+            data.sort((a, b) => b.avgRating - a.avgRating);
+        })
     }
 
     render (selector, RENDER_COL = 3) {
         // RENDER_COL = количество выводимых статей в блоке
-        this.posts.slice(0, RENDER_COL).forEach((item, i) => {
-            const renderTopics = document.createElement('div');
-            renderTopics.innerHTML = '<span class="badge badge-secondary">' +
-                                    item.topics.join('</span> <span class="badge badge-secondary">') +
-                                    '</span>';
-            document.querySelectorAll ('.' + selector + ' > .blog_h3')[i].innerHTML = item.title;
-            if (item.avgRating) {
-                document.querySelectorAll ('.' + selector + ' > .rating')[i].innerHTML = 'Rating: ' + item.avgRating.toFixed(1);
-            }
-            document.querySelectorAll ('.' + selector + ' > .topics')[i].append(renderTopics);
+        this.posts.then(data => {
+            data.slice(0, RENDER_COL).forEach((item, i) => {
+                const renderTopics = document.createElement('div');
+                renderTopics.innerHTML = '<span class="badge badge-secondary">' +
+                                        item.topics.join('</span> <span class="badge badge-secondary">') +
+                                        '</span>';
+                document.querySelectorAll ('.' + selector + ' > .blog_h3')[i].innerHTML = item.title;
+                if (item.avgRating) {
+                    document.querySelectorAll ('.' + selector + ' > .rating')[i].innerHTML = 'Rating: ' + item.avgRating.toFixed(1);
+                }
+                document.querySelectorAll ('.' + selector + ' > .topics')[i].append(renderTopics);
+            })
         })
     }
 }
 
-// function getDataFromServerApi (url) {
-//     data = fetch(url)
-//     .then(response => response.json())
-//     .catch(error => alert(error));
-//     return data;
-// }
+//============ < КЛАССЫ ============//
 
-// const blogPosts = getDataFromServerApi('https://my-json-server.typicode.com/dahaka-vas/EPM-FE2019/posts')
-// .then(data => {
-//     posts = new Blog(data);
-//     posts.avgRatingSort();
-//     posts.render('blog-1_section');
-// })
-// .catch(error => {
-//     alert(error);
-// });
-const BLOG_URL = 'https://my-json-server.typicode.com/dahaka-vas/EPM-FE2019/posts';
-const blogPosts = new Blog(BLOG_URL);
+
+
+//============ КОД > ============//
+
+const blogPosts = new Blog();
+// blogPosts.fillPosts();
 blogPosts.avgRatingSort();
 blogPosts.render('blog-1_section');
+
+//============ < КОД ============//
+
+
+
+// const BLOG_URL = 'https://my-json-server.typicode.com/dahaka-vas/EPM-FE2019/posts';
+// const blogPosts = new Blog(BLOG_URL);
+// blogPosts.avgRatingSort();
+// blogPosts.render('blog-1_section');
